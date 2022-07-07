e514d23415c210dac18110891559800b3c2c8b85
Ahnsang0915 <tmqjf4921@gmail.com>
2022-07-07 17:12:06 +0900

    Modify models.py indentation

--- students/sanghyun/users/models.py
+++ students/sanghyun/users/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=250)
+    name          = models.CharField(max_length=50)
+    email         = models.CharField(max_length=100, unique=True)
+    password      = models.CharField(max_length=250)
     mobile_number = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at    = models.DateTimeField(auto_now_add=True)
+    updated_at    = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
