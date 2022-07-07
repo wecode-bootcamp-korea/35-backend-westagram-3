31bec0a2f4f90e356b2d3ec9a391456f5406bc70
김민지 <minji@gimminjiui-MacBookAir.local>
2022-07-07 15:22:21 +0900

    Modify: models.py attribute

--- students/minji/users/models.py
+++ students/minji/users/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     name        = models.CharField(max_length=20)
     email       = models.CharField(max_length=45)
     password    = models.CharField(max_length=45)
-    phoneNumber = models.IntegerField()
+    phoneNumber = models.CharField(max_length=11)
 
     class Meta:
         db_table = 'users'
