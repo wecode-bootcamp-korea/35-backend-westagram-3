a16bac8d8a7e98dca6830d46bfd7c6dc209c66fa
조예슬 <eungang3@naver.com>
2022-07-07 16:25:24 +0900

    Add: [Mission 3] view 작성중

--- students/yeseul/users/views.py
+++ students/yeseul/users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+import json 
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from users.models import User
+
+class SignUpView(View):
+    # http -v POST 127.0.0.1:8000/sign_up name='영희' email='younghee@naver.com' password='1234' phone_number='01012341234'
+    def post(self, request):
+        data = json.loads(request.body)
+        
+        return JsonResponse({'results' : 1}, status = 200)
