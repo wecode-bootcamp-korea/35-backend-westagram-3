f3c54dcdb997230f6c243674eb3de1dca96cd260
김민지 <minji@gimminjiui-MacBookAir.local>
2022-07-07 20:33:09 +0900

    Modify: models.py

--- students/minji/users/models.py
+++ students/minji/users/models.py
@@ -5,8 +5,8 @@ class User(models.Model):
     email       = models.CharField(max_length=45,unique=True)
     password    = models.CharField(max_length=200)
     phoneNumber = models.CharField(max_length=11)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
