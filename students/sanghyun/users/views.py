c33dce70469c57b9e93c1901fd16b603f2425a7d
Ahnsang0915 <tmqjf4921@gmail.com>
2022-07-08 16:56:37 +0900

    Error message 수정

--- students/sanghyun/users/views.py
+++ students/sanghyun/users/views.py
@@ -16,11 +16,11 @@ class SignUpView(View):
             email    = data['email']
             password = data['password']
             if not re.match(check_email, email):
-                return JsonResponse({'message' : '메일주소 확인 바랍니다.'}, status=404)
+                return JsonResponse({'message' : '메일주소 확인 바랍니다.'}, status=400)
             if not re.match(check_password, password):
-                return JsonResponse({'message' : '비밀번호는 8글자 이상, 문자, 숫자, 특수문자 조합이어야 합니다.'}, status=404)
+                return JsonResponse({'message' : '비밀번호는 8글자 이상, 문자, 숫자, 특수문자 조합이어야 합니다.'}, status=400)
             if User.objects.filter(email = email).exists():
-                return JsonResponse({'message' : '이미 사용중인 이메일 입니다.'}, status=404)
+                return JsonResponse({'message' : '이미 사용중인 이메일 입니다.'}, status=400)
             User.objects.create(
                 name          = data['name'],
                 email         = data['email'],
