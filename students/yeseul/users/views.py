a5438a1c796e4750bcde68bf746aa3842ef3ee93
조예슬 <eungang3@naver.com>
2022-07-08 13:15:39 +0900

    [Mission 3] 회원가입 기능 구현중

--- students/yeseul/users/views.py
+++ students/yeseul/users/views.py
@@ -1,4 +1,5 @@
 import json 
+import re
 
 from django.http  import JsonResponse
 from django.views import View
@@ -6,8 +7,37 @@ from django.views import View
 from users.models import User
 
 class SignUpView(View):
-    # http -v POST 127.0.0.1:8000/sign_up name='영희' email='younghee@naver.com' password='1234' phone_number='01012341234'
     def post(self, request):
-        data = json.loads(request.body)
+        try:
+            request_data = json.loads(request.body)
+            name         = request_data['name']
+            email        = request_data['email']
+            password     = request_data['password']
+            phone_number = request_data['phone_number'] 
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
         
-        return JsonResponse({'results' : 1}, status = 200)
+        # 이미 존재하는 이메일이 있는지 확인
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'message' : 'same email exists'}, status = 400)
+            
+        # email validation
+        email_regex = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+        if email_regex.match(email) == None:
+            return JsonResponse({'message' : 'email validation failed'}, status = 400)
+        
+        # password validation(8자리 이상, 최소 하나의 문자, 하나의 숫자, 하나의 특수문자)
+        password_regex = re.compile('''^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@!%*?&!"£$%^&*()_+{}:@~<>?|=[\];'#,.\/\\-])[\S]{8,}$''')
+        if password_regex.match(password) == None:
+            return JsonResponse({'message' : 'password validation failed'}, status = 400)
+
+        User.objects.create(
+            name         = name,
+            email        = email,
+            password     = password,
+            phone_number = phone_number
+        )
+        
+        return JsonResponse({'message' : 'SUCCESS'}, status = 201)
+
+
