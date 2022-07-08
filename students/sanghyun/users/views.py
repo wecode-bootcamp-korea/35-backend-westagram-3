d152f6c290674d38c6aa68aa666d05b30f3ccef9
Ahnsang0915 <tmqjf4921@gmail.com>
2022-07-08 17:55:23 +0900

    views.py 줄간격 수정

--- students/sanghyun/users/views.py
+++ students/sanghyun/users/views.py
@@ -6,21 +6,37 @@ from django.views import View
 
 from users.models import User
 
+
+check_email    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+check_password = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
+
 class SignUpView(View):
     def post(self, request):
-        data           = json.loads(request.body)
-        check_email    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-        check_password = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
+        data         = json.loads(request.body)
         try:
             password = data['password']
             email    = data['email']
             password = data['password']
+
+
             if not re.match(check_email, email):
-                return JsonResponse({'message' : '메일주소 확인 바랍니다.'}, status=400)
+                return JsonResponse(
+                    {'message' : '메일주소 확인 바랍니다.'}, status=400
+                    )
+
+
             if not re.match(check_password, password):
-                return JsonResponse({'message' : '비밀번호는 8글자 이상, 문자, 숫자, 특수문자 조합이어야 합니다.'}, status=400)
+                return JsonResponse(
+                    {'message' : '비밀번호는 8글자 이상, 문자, 숫자, 특수문자 조합이어야 합니다.'}, status=400
+                    )
+
+
             if User.objects.filter(email = email).exists():
-                return JsonResponse({'message' : '이미 사용중인 이메일 입니다.'}, status=400)
+                return JsonResponse(
+                    {'message' : '이미 사용중인 이메일 입니다.'}, status=400
+                    )
+
+
             User.objects.create(
                 name          = data['name'],
                 email         = data['email'],
@@ -28,5 +44,7 @@ class SignUpView(View):
                 mobile_number = data['mobile_number']
             )
             return JsonResponse({'message' : 'SUCCESS'}, status=201)
+
+
         except KeyError:
             return JsonResponse({'message' : 'Key_ERROR'}, status=400)
