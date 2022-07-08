0139989e645a3a2108e263a650c767c3c57d0910
Ahnsang0915 <tmqjf4921@gmail.com>
2022-07-08 16:51:49 +0900

    Add views.py, urls.py

--- students/sanghyun/users/views.py
+++ students/sanghyun/users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import json
+import re
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from users.models import User
+
+class SignUpView(View):
+    def post(self, request):
+        data           = json.loads(request.body)
+        check_email    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        check_password = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
+        try:
+            password = data['password']
+            email    = data['email']
+            password = data['password']
+            if not re.match(check_email, email):
+                return JsonResponse({'message' : '메일주소 확인 바랍니다.'}, status=404)
+            if not re.match(check_password, password):
+                return JsonResponse({'message' : '비밀번호는 8글자 이상, 문자, 숫자, 특수문자 조합이어야 합니다.'}, status=404)
+            if User.objects.filter(email = email).exists():
+                return JsonResponse({'message' : '이미 사용중인 이메일 입니다.'}, status=404)
+            User.objects.create(
+                name          = data['name'],
+                email         = data['email'],
+                password      = data['password'],
+                mobile_number = data['mobile_number']
+            )
+            return JsonResponse({'message' : 'SUCCESS'}, status=201)
+        except KeyError:
+            return JsonResponse({'message' : 'Key_ERROR'}, status=400)
