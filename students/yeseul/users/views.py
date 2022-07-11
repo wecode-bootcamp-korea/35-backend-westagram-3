39dad0dd6a1cb376d22e1005bfce7df5a9427c7a
조예슬 <eungang3@naver.com>
2022-07-11 15:12:32 +0900

    [Mission 4] 로그인 기능 구현 - try문 반환값 수정

--- students/yeseul/users/views.py
+++ students/yeseul/users/views.py
@@ -50,11 +50,13 @@ class LoginView(View):
 
             if not User.objects.get(email=email).password == password:
                 return JsonResponse({'message' : 'INVALID_USER'}, status = 401)
-
+            
+            return JsonResponse({'message' : 'SUCCESS'}, status = 200)
+        
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
-            
+
         except ObjectDoesNotExist:
             return JsonResponse({'message' : 'INVALID_USER'}, status = 401)
 
-        return JsonResponse({'message' : 'SUCCESS'}, status = 200)
+        
