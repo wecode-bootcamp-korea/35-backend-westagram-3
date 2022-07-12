a93b04334282b973438925a2860fb60466d2416d
Kim minji <enddl3224@gmail.com>
2022-07-12 21:32:20 +0900

    Modify: algorithm 환경변수로 설정

--- students/minji/users/views.py
+++ students/minji/users/views.py
@@ -3,10 +3,11 @@ import re
 import bcrypt
 import jwt
 
-
 from django.http      import JsonResponse
 from django.views     import View
 from users.models     import User
+
+from my_settings      import ALGORITHM
 
 REGEX_EMAIL    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
 REGEX_PASSWORD = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
@@ -56,17 +57,17 @@ class LogInView(View):
 
 
         try:
-            encoded_jwt = jwt.encode({'id':User.objects.get(email=email).id},'secret',algorithm='HS256')
+            encoded_jwt = jwt.encode({'id':User.objects.get(email=email).id},'secret',algorithm=ALGORITHM)
             
             if User.objects.filter(email = email, password=password):
                 return JsonResponse({"message": "SUCCESS"}, status = 200)
 
             if not bcrypt.checkpw(password.encode("utf-8"), User.objects.get(email=email).password.encode("utf-8")):
-                return JsonResponse({"MESSAGE": "INVALID_USER"}, status=401)
+                return JsonResponse({"MESSAGE": "INVALID_USER"}, status = 401)
 
             return JsonResponse({"message": encoded_jwt}, status = 200)
 
         except KeyError:
-            return JsonResponse({"message": "KEY_ERROR"},status = 400)
+            return JsonResponse({"message": "KEY_ERROR"}, status = 400)
         
         
